29acf03af0b33461dfaf7faac555c1bf708356c4
Axhutoxh <Ashutosh.chandra.520@gmail.com>
2022-02-24 23:43:52 +0530

    #intialize capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.hale',
+  appName: 'android-ui',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
